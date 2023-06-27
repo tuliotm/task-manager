dd751cee43034655d6d44de61bd88a6bf66d771b
tuliotm <tulioteodoromanso@gmail.com>
2023-06-27 10:40:50 -0300

    Some changes in routes and failures in frontend...

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_task, only: %i[ edit update destroy ]
 
   # GET /tasks or /tasks.json
   def index
     @tasks = Task.all
-  end
-
-  # GET /tasks/1 or /tasks/1.json
-  def show
   end
 
   # GET /tasks/new
@@ -25,9 +21,10 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+        format.html { redirect_to tasks_path, notice: "Tarefa criada com sucesso." }
         format.json { render :show, status: :created, location: @task }
       else
+        flash.now[:alert] = @task.errors.full_messages.to_sentence
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
@@ -38,9 +35,10 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
+        format.html { redirect_to tasks_path, notice: "Tarefa atualizada com sucesso." }
         format.json { render :show, status: :ok, location: @task }
       else
+        flash.now[:alert] = @task.errors.full_messages.to_sentence
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @task.errors, status: :unprocessable_entity }
       end
@@ -52,7 +50,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+      format.html { redirect_to tasks_url, notice: "Tarefa apagada com sucesso." }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root "tasks#index"
 
-  resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tasks, except: %i[show]
 end
