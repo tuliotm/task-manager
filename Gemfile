6f45f7e4cfdf964861ae6cb8732e358c7b862075
tuliotm <tulioteodoromanso@gmail.com>
2023-06-27 10:43:33 -0300

    Add i18n

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "rails-i18n", "~> 7.0"
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module GerenciadorDeTarefas
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.default_locale = 'pt-BR'
+    config.time_zone = 'Brasília'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
